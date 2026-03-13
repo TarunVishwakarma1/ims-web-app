9f93fd78bf26d85c8b7c8f802e66757782ea955a
Tarun Vishwakarma <tarunvishwakarma81@gmail.com>
2026-03-13 21:52:05 +0530

    Completed Adding light and dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { ModeToggle } from "@/components/ui/mode-toggle";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1>Home</h1>
+      <ModeToggle />
     </div>
 
   );
